b19a57d507383a654a0e5578608dc13cc79ac6f0
Paulo Lourenco <paulo@lourenco.net.au>
2017-12-14 15:29:09 +1100

    looping function to retrieve price data

--- btcmarkets/app.js
+++ btcmarkets/app.js
@@ -7,27 +7,39 @@ var client = new BTCMarkets(secrets.api_key, secrets.api_secret);
 var numberConverter = 100000000;    // one hundred million
 
 // get latest prices
-client.getTick("BTC", "AUD", function(err, data)
-{
-    console.log('bid ' + data.bestBid + ' ask ' + data.bestAsk + ' last price ' + data.lastPrice);
-});
+// client.getTick("BTC", "AUD", function(err, data)
+// {
+//     console.log('BTC / AUD bid ' + data.bestBid + ' ask ' + data.bestAsk + ' last price ' + data.lastPrice);
+//     console.log(data);
+// });
 
-client.getAccountBalances(function(err, data)
-{
-    if (err){
-        console.log(err);
-    }
-    else {
-        data.forEach(function(account)
-        {
-            console.log(account.currency + ' balance ' + account.balance / numberConverter + ' pending ' + account.pendingFunds / numberConverter);
-        });
-    }
-});
+// client.getAccountBalances(function(err, data)
+// {
+//     if (err){
+//         console.log(err);
+//     }
+//     else {
+//         data.forEach(function(account)
+//         {
+//             console.log(account.currency + ' balance ' + account.balance / numberConverter + ' pending ' + account.pendingFunds / numberConverter);
+//         });
+//     }
+// });
 
-client.getTradingFee("BTC", "AUD", function(err, data)
-{
-    if (!err) {
-        console.log("BTC/AUD trading fee: " + data);
-    }
-});
+// client.getTradingFee("BTC", "AUD", function(err, data)
+// {
+//     if (!err) {
+//         console.log("BTC/AUD trading fee: " + data);
+//     }
+// });
+
+function logPriceToFile(btcclient, crypto, currency) {
+    btcclient.getTick(crypto, currency, function(err, data)
+    {
+        if(!err){
+            console.log(data);
+        }
+    });
+}
+
+setInterval(logPriceToFile.bind(null, client, "BTC", "AUD"), 5000);
