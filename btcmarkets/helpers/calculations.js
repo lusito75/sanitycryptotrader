cc4e8da9f9fa3836bb3d34253e9c911866923feb
Paulo Lourenco <paulo@lourenco.net.au>
2017-12-19 10:43:38 +1100

    formatting

--- btcmarkets/helpers/calculations.js
+++ btcmarkets/helpers/calculations.js
@@ -3,41 +3,39 @@ var Calc   = require('../models/calcs');
 
 var helperObj = {};
 
-
-
 helperObj.updateCalc = function (crypto, min, max, latest){
     // retrieve latest calcs for crypto
     Calc.findOneAndUpdate({'instrument': crypto}, {'instrument': crypto}, {upsert:true}, function(err, myCalc){
-    if (err) {
-        console.log(err.message);
-    } else {
-        // update longTermMin and longTermMax if relevant
-        console.log(crypto + ' recent min: ' + min + ' recent max: ' + max + ' latest: ' + latest);
-        if (!myCalc.longTermMin) {
-            myCalc.longTermMin = min;
-        }
-        else if (min < myCalc.longTermMin){
-            myCalc.longTermMin = min;
-        }
-        if (!myCalc.longTermMax) {
-            myCalc.longTermMax = max;
-        }
-        else if (max > myCalc.longTermMax){
-            myCalc.longTermMax = max;
-        }
-        if (myCalc.lastAction === "buy"){
-            var profit = ((latest - myCalc.lastPrice) / myCalc.lastPrice)*100;
-            if (profit >= 10){
-                console.log(crypto + " SELL for " + profit +"% @" + latest);
-                //update lastPrice, update lastAction, average out running profit
-                myCalc.lastPrice = latest;
-                myCalc.lastAction = "sell";
-                var avg = (myCalc.runningProfit + profit) / 2;
-                myCalc.runningProfit = avg;
-                    }
+        if (err) {
+            console.log(err.message);
+        } else {
+            // update longTermMin and longTermMax if relevant
+            console.log(crypto + ' recent min: ' + min + ' recent max: ' + max + ' latest: ' + latest);
+            if (!myCalc.longTermMin) {
+                myCalc.longTermMin = min;
+            }
+            else if (min < myCalc.longTermMin){
+                myCalc.longTermMin = min;
+            }
+            if (!myCalc.longTermMax) {
+                myCalc.longTermMax = max;
+            }
+            else if (max > myCalc.longTermMax){
+                myCalc.longTermMax = max;
+            }
+            if (myCalc.lastAction === "buy"){
+                var profit = ((latest - myCalc.lastPrice) / myCalc.lastPrice)*100;
+                if (profit >= 10){
+                    console.log(crypto + " SELL for " + profit +"% @" + latest);
+                    //update lastPrice, update lastAction, average out running profit
+                    myCalc.lastPrice = latest;
+                    myCalc.lastAction = "sell";
+                    var avg = (myCalc.runningProfit + profit) / 2;
+                    myCalc.runningProfit = avg;
                 }
-                else if (myCalc.lastAction === "sell"){
-                    if (latest <= min){
+            }
+            else if (myCalc.lastAction === "sell"){
+                if (latest <= min){
                     console.log(crypto + " lowest value in last week: @" + latest + " BUY moderate");
                     if (latest <= myCalc.longTermMin) {
                         console.log(crypto + " lowest value on record: @" + latest + " BUY strong");
@@ -65,6 +63,5 @@ helperObj.updateCalc = function (crypto, min, max, latest){
         }
     });
 }
-
 
 module.exports = helperObj;
