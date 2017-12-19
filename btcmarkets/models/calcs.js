95499b33558c0e4ad628d272ae259ebee2ab5d9c
Paulo Lourenco <paulo@lourenco.net.au>
2017-12-20 10:42:01 +1100

    document calc schema

--- btcmarkets/models/calcs.js
+++ btcmarkets/models/calcs.js
@@ -3,16 +3,16 @@ var timestamps = require('mongoose-timestamp');
 
 //SCHEMA SETUP
 var calcSchema = new mongoose.Schema({
-    longTermMin: Number,
-    longTermMax: Number,
-    trend: String,
-    lastAction: String,
-    recommendedAction: String,
-    previousPrice: Number,
-    lastTradedPrice: Number,
-    percentGain: Number,
-    runningProfit: Number,
-    instrument: String,
+    longTermMin: Number,        // all time minimum value since records began
+    longTermMax: Number,        // all time maximum value since records began
+    trend: String,              // "d" (down) "u" (up) "." steady
+    lastAction: String,         // "buy" or "sell"
+    recommendedAction: String,  // "buy" or "sell"
+    previousPrice: Number,      // previous sample price
+    lastTradedPrice: Number,    // last price achieve in a buy or sell action
+    percentGain: Number,        // gain/loss since last trade
+    runningProfit: Number,      // accumulated profit over several trades
+    instrument: String,         // crypto currency, eg "BTC", "ETH", "LTC", "BCH", "XRP"
 });
 
 calcSchema.plugin(timestamps);
