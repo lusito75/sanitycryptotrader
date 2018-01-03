18af4426f346a9e9d71648d038e1bf45f5e42971
Paulo Lourenco <paulo@lourenco.net.au>
2018-01-03 17:20:08 +1100

    add route to retrieve Calcs, add display form

--- btcmarkets/routes/index.js
+++ btcmarkets/routes/index.js
@@ -1,9 +1,17 @@
 var express  = require('express');
 var router   = express.Router();
 
+var Calc    = require('../models/calcs');
+
 router.get("/", function(req, res){
     // res.send("Crypto Trader landing page");
-    res.render("main");
+    Calc.find({}, function(err, allCalcs){
+        if (err) {
+            console.log(err.message);
+        } else {
+            res.render("index", {calcs: allCalcs});
+        }
+    });
 });
 
 
